507a72e2c239891b100e5f08bb7d608da2d05df3
Moises <ve11621@innovaccion.mx>
2022-09-20 13:41:39 -0500

    JUST THE SHELL

---
+++ src/test/java/com/jdbctemplatepractice/jdbctemplatepractice/JdbctemplatepracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbctemplatepractice.jdbctemplatepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbctemplatepracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
